ae9ccfd8c63bc98f49db9dd2782eed9860ce9691
dasarathaRamiReddy047 <132756705+dasarathaRamiReddy047@users.noreply.github.com>
2023-08-03 00:10:44 +0530

    first commit

--- AadharTestNg/src/test/java/com/LoginAndRegistrationTest.java
+++ AadharTestNg/src/test/java/com/LoginAndRegistrationTest.java
@@ -2,12 +2,14 @@ package com;
 
 
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.AssertJUnit;
 
-import org.openqa.selenium.Alert;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -50,8 +52,7 @@ public class LoginAndRegistrationTest {
         passwordInput.sendKeys("123456");
         loginButton.click();
         Thread.sleep(2000);
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+     
 
         String expectedTitle = "MaadharAppProject"; 
         String actualTitle = driver.getTitle();
@@ -77,12 +78,67 @@ public class LoginAndRegistrationTest {
         passwordInput.sendKeys("123456");
         phoneInput.sendKeys("1234567890");
         
+  JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        
+        for (int i = 0; i < 6; i++) {
+            js.executeScript("window.scrollBy(0,200)", "");
+            System.out.println("System Scrolling " + i);
+        }
+        
         Thread.sleep(6000);
         registerButton.click();
 
         String expectedTitle = "MaadharAppProject"; 
         String actualTitle = driver.getTitle();
         AssertJUnit.assertEquals(actualTitle, expectedTitle);
+    }
+    
+    
+    @Test(priority = 4)
+    public void testNewAadharCardApplication() throws InterruptedException {
+    	String url="http://localhost:4200/service";
+        driver.get(url);
+        WebElement applyButton = driver.findElement(By.cssSelector(".btn.bt-info.button"));
+        applyButton.click();
+ 
+        
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        
+
+        WebElement nameInput = driver.findElement(By.name("name"));
+        nameInput.sendKeys("John Doe");
+
+        WebElement dobInput = driver.findElement(By.name("date"));
+        dobInput.sendKeys("1990-01-01");
+
+        WebElement emailInput = driver.findElement(By.name("email"));
+        emailInput.sendKeys("johndoe@example.com");
+
+        WebElement addressInput = driver.findElement(By.name("aadrees"));
+        addressInput.sendKeys("123 Main St");
+
+        WebElement phoneInput = driver.findElement(By.name("phone"));
+        phoneInput.sendKeys("1234567890");
+        
+        Thread.sleep(6000);
+        
+        for(int i=0; i<6 ; i++) {
+            js.executeScript("window.scrollBy(0,600)", "");
+            System.out.println("System Scrolling "+ i);
+        }
+
+        WebElement genderMaleInput = driver.findElement(By.id("male"));
+        genderMaleInput.click();
+        js.executeScript("window.scrollBy(0,450)", "");
+
+        
+        WebElement submitButton = driver.findElement(By.cssSelector(".btn.btn-primary"));
+        Thread.sleep(2000);
+        submitButton.click();
+        
+      
+
     }
 
 
